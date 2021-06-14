67a1e4e993f569c98883c8e5bcef5d5f5de9f0f3
Gajuri Mahaju <coolgoose74@gmail.com>
2021-06-14 13:03:02 -0500

    jenkins pipeline script updated for demo

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
+                git url: 'https://github.com/coolgoose74/jgsu-spring-petclinic.git', branch: 'main'
             }            
         }
         stage('Build') {
@@ -18,14 +18,14 @@ pipeline {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
                 }
-                changed {
+                /* changed {
                     emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
                         body: "Please go to ${BUILD_URL} and verify the build", 
                         attachLog: true, 
                         compressLog: true, 
                         to: "test@jenkins",
                         recipientProviders: [upstreamDevelopers(), requestor()]
-                }
+                } */
             }
         }
     }
