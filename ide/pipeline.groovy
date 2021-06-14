80e309047908f5a20f9ac0bd9259c57397726cb4
Gajuri Mahaju <coolgoose74@gmail.com>
2021-06-14 16:56:30 -0500

    intial email notification test

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -10,22 +10,22 @@ pipeline {
         stage('Build') {
             steps {
                 sh './mvnw clean package'
-                //sh 'false' // true
+                //sh 'false' // true  #for demo purpose  to avoid test execution during demo, comment above mav clean package...
             }
         
             post {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
-                }
-                /* changed {
+                //}
+                 //changed {
                     emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
                         body: "Please go to ${BUILD_URL} and verify the build", 
                         attachLog: true, 
                         compressLog: true, 
                         to: "test@jenkins",
                         recipientProviders: [upstreamDevelopers(), requestor()]
-                } */
+                } 
             }
         }
     }
